e1af3ae9fd357eb3176e563b96fb544b0f56e81f
drvinceknight <vincent.knight@gmail.com>
2013-01-14 17:33:17 +0000

    Added description to Exercise.py

--- Code/Exercise.py
+++ Code/Exercise.py
@@ -1,2 +1,8 @@
+"""
+This python file contains a dictionary listing course codes to code names and a list containing names of students and the course codes on which they're enrolled. The solution file shows an OOP approach to obtaining the number of people enrolled on each course as well as the names of courses on which everyone is enrolled.
+
+"""
+
+
 Course_Dictionary = {1: "Math", 2: "English", 3: "French", 4: "Physics", 5: "PE", 6: "Biology", 7: "History", 8: "Geography"}
 Student_List = [["Adam", [1, 2, 5]], ["Zoe", [4, 3, 2]], ["Ben", [7]], ["Thomas", []], ["Meryl", [2, 3]], ["James", [6, 7, 2]], ["Leanne", []], ["Angelico", [1]], ["Izabela", [1, 2, 3, 4, 5]], ["Lisa", [1, 8]], ["Penny", [1, 2, 3]]]
