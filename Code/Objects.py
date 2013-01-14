1c8a30c9362d0a10f6e2c256c63662f03ffe2184
drvinceknight <vincent.knight@gmail.com>
2013-01-14 11:03:42 +0000

    Edited some basic file, need to add code to them, add example description, and pep8 solution file

--- Code/Objects.py
+++ Code/Objects.py
@@ -1,70 +1,76 @@
 #We will define a new class called "Student"
+
+
 class Student():
     pass
 
-#We create Vince, an instance of Student:
-Vince=Student()
+#We create vince,  an instance of Student:
+vince = Student()
 
-#We can call Vince to see what "it" is:
-print Vince
+#We can call vince to see what "it" is:
+print vince
+
 
 #We modify our class Student so that our object has numerous attributes:
 class Student():
-    courses=["Biology","Mathematics","English"]
-    age=5
-    sex="Male"
-#Let us now create Vince again:
-Vince=Student()
+    courses = ["Biology", "Mathematics", "English"]
+    age = 5
+    sex = "Male"
+#Let us now create vince again:
+vince = Student()
 
 #We can take a look at the values of each attribute:
-print Vince.courses
-print Vince.age
-print Vince.sex
+print vince.courses
+print vince.age
+print vince.sex
 
 #We can add a new course:
-Vince.courses.append("French")
-print Vince.courses
+vince.courses.append("French")
+print vince.courses
 #We can change the value of the age:
-Vince.age=28
-print Vince.age
+vince.age = 28
+print vince.age
 #We can change the value of sex:
-Vince.sex="M"
-print Vince.sex
+vince.sex = "M"
+print vince.sex
 
 
 class Student():
-    courses=["Biology","Mathematics","English"]
-    age=5
-    sex="Male"
+    courses = ["Biology", "Mathematics", "English"]
+    age = 5
+    sex = "Male"
+
 #We can define methods within our class:
     def have_a_birthday(self):
-        self.age+=1
+        self.age += 1
  #Let us see how this works:
-Vince=Student()
-print Vince.age
-Vince.have_a_birthday()
-print Vince.age
+vince = Student()
+print vince.age
+vince.have_a_birthday()
+print vince.age
+
 
 #The init method is a special method that allows us to pass arguments when we generate an instance of a class.
 class Student():
-    def __init__(self,courses,age,sex):
-        self.courses=courses
-        self.age=age
-        self.sex=sex
+    def __init__(self, courses, age, sex):
+        self.courses = courses
+        self.age = age
+        self.sex = sex
+
 #We can define methods within our class:
     def have_a_birthday(self):
-        self.age+=1
+        self.age += 1
 #Let us see how this works:
-Vince=Student(["Biology","Math"],28,"Male")
-print Vince.courses
-print Vince.age
-print Vince.sex
+vince = Student(["Biology", "Math"], 28, "Male")
+print vince.courses
+print vince.age
+print vince.sex
 
 
 #We can use a class to create new classes:
 class Math_Student(Student):
-    favorite_class="Mathematics"
+    favorite_class = "Mathematics"
 
-Vince=Math_Student(["Math,Geography"],10,"Male")
-print Vince.age
-print Vince.favorite_class
+vince = Math_Student(["Math, Geography"], 10, "Male")
+print vince.age
+print vince.favorite_class
