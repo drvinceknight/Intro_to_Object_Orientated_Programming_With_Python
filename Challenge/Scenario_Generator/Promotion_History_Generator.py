e5373cad245422f0be1af601f30e6d87ddfb5e47
drvinceknight <vincent.knight@gmail.com>
2013-01-14 14:34:24 +0000

    PEP8'd Scenario Generator file

--- Challenge/Scenario_Generator/Promotion_History_Generator.py
+++ Challenge/Scenario_Generator/Promotion_History_Generator.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python
 import csv
-import random
 import os
+import random
 import sys
 
 #random.seed(1)
 
 
-files_in_directory=os.listdir(".")
+files_in_directory = os.listdir(".")
 
-if len(sys.argv)==1:
+if len(sys.argv) == 1:
     if "Stock_Report.csv" not in files_in_directory:
         print "-----"
         print ""
@@ -17,7 +17,7 @@ if len(sys.argv)==1:
         print ""
         print "-----"
     else:
-        Stock_Report="Stock_Report.csv"
+        stock_report = "Stock_Report.csv"
     if "Sale_History.csv" not in files_in_directory:
         print "-----"
         print ""
@@ -25,44 +25,44 @@ if len(sys.argv)==1:
         print ""
         print "-----"
     else:
-        Sale_History="Sale_History.csv"
+        sale_history = "Sale_History.csv"
 else:
-    Stock_Report=argv[1]
-    Sale_History=argv[2]
+    stock_report = sys.argv[1]
+    sale_history = sys.argv[2]
 
-outfile=open(Sale_History,"rb")
-dates=csv.reader(outfile)
-dates=[row for row in dates]
-dates=list(set([eval(e[0]) for e in dates[1:]]))
+outfile = open(sale_history, "rb")
+dates = csv.reader(outfile)
+dates = [row for row in dates]
+dates = list(set([eval(e[0]) for e in dates[1:]]))
 outfile.close()
 
-outfile=open(Stock_Report,"rb")
-departments=csv.reader(outfile)
-departments=[row for row in departments]
-departments=list(set([e[0] for e in departments[1:]]))
+outfile = open(stock_report, "rb")
+departments = csv.reader(outfile)
+departments = [row for row in departments]
+departments = list(set([e[0] for e in departments[1:]]))
 outfile.close()
 
-Promotions=[i*10 for i in range(1,10)]
+promotions = [i * 10 for i in range(1, 10)]
 
-promotion_dict={}
+promotion_dict = {}
 for e in departments:
-    promotion_dict[e]=random.random()
+    promotion_dict[e] = random.random()
 
-filename="Promotions_History.csv"
+filename = "Promotions_History.csv"
 
-file=open(filename,"wb")
-output=csv.writer(file)
-output.writerow(["Date","Department","Percent Reduction"])
-k=0
+file = open(filename, "wb")
+output = csv.writer(file)
+output.writerow(["Date", "Department", "Percent Reduction"])
+k = 0
 for i in dates:
     for e in departments:
-        if random.random()<promotion_dict[e]:
-            k+=1
-            output.writerow([i,e,random.choice(Promotions)])
+        if random.random() < promotion_dict[e]:
+            k += 1
+            output.writerow([i, e, random.choice(promotions)])
 file.close()
 
 print "-----------------------------"
 print ""
-print "Output file: %s created with %s sales over %s days."%(filename,k,len(dates))
+print "Output file: %s created with %s sales over %s days." % (filename, k, len(dates))
 print ""
 print "-----------------------------"
--- Challenge/Scenario_Generator/Sale_History_Generator.py
+++ Challenge/Scenario_Generator/Sale_History_Generator.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
-import random
-import sys
+from math import exp
 import csv
 import os
-from math import exp
+import random
+import sys
 """
 This file generates a data file to be used as the Sales History for the challenge. The code generates a number of customers for each day (the total number of days is an input parameter) sampled from a Poisson distribution.
 
@@ -15,32 +15,34 @@ One can input the name of the file to be produced (Default: "Sale_History.csv").
 #Uncomment if you want to stick to a random seed (ie recreate same Sales History again and again).
 #random.seed(1)
 
+
 def poisson_sample(lmbda):
     """
     An algorithm of Knuth to sample numbers from a Poisson distribution.
     """
-    L,k,p=exp(-lmbda),0,1
-    while p>L:
-        p*=random.random()
-        k+=1
-    return k-1
+    L, k, p = exp(-lmbda), 0, 1
+    while p > L:
+        p *= random.random()
+        k += 1
+    return k - 1
 
 
-def import_file(Stock_Report="Stock_Report.csv"):
+def import_file(stock_report="Stock_Report.csv"):
     """
     A function to import a Stock Report file. It returns a dictionary with keys the departments and values the list of available products.
     """
-    outfile=open(Stock_Report,"rb")
-    data=csv.reader(outfile)
-    data=[row for row in data]
+    outfile = open(stock_report, "rb")
+    data = csv.reader(outfile)
+    data = [row for row in data]
     outfile.close()
-    P={}
+    P = {}
     for row in data[1:]:
         if row[0] in P:
             P[row[0]].append(row[1])
         else:
-            P[row[0]]=[row[1]]
+            P[row[0]] = [row[1]]
     return P
+
 
 class Customer():
     """
@@ -52,85 +54,83 @@ class Customer():
 
     This class has a method "new_basket" which regenerates a basket if needed.
     """
-    def __init__(self,Products,Buying_Probability=.8,Loyal_Probability=.5):
+    def __init__(self, products, buying_probability=.8, loyal_probability=.5):
         #Create a dictionary that will contain the buying probability for each department for this customer.
-        self.buying_prob={}
-        for e in Products:
-            dep_buying_prob=Buying_Probability*random.random()
-            temp_dict={}
-            for i in Products[e]:
-                temp_dict[i]=random.random()*dep_buying_prob
-            self.buying_prob[e]=temp_dict
+        self.buying_prob = {}
+        for e in products:
+            dep_buying_prob = buying_probability * random.random()
+            temp_dict = {}
+            for i in products[e]:
+                temp_dict[i] = random.random() * dep_buying_prob
+            self.buying_prob[e] = temp_dict
 
-        self.basket=[]
-        for e in Products:
-            for i in Products[e]:
-                if random.random()<self.buying_prob[e][i]:
+        self.basket = []
+        for e in products:
+            for i in products[e]:
+                if random.random() < self.buying_prob[e][i]:
                     self.basket.append(i)
 
-        self.loyal=False
-        if random.random()<Loyal_Probability:
-            self.loyal=True
+        self.loyal = False
+        if random.random() < loyal_probability:
+            self.loyal = True
 
-    def new_basket(self,Products):
-        self.basket=[]
-        for e in Products:
-            for i in Products[e]:
-                if random.random()<self.buying_prob[e][i]:
+    def new_basket(self, products):
+        self.basket = []
+        for e in products:
+            for i in products[e]:
+                if random.random() < self.buying_prob[e][i]:
                     self.basket.append(i)
-
-
 
 
 class Sale_History():
     """
     Class for the Sale History. On initialisation it creates customers for each day of sale (the number of customers is randomly sampled from the Poisson distribution). There is a method "create_file" that outputs a csv file.
     """
-    def __init__(self,Products,Mean_Customers_Per_Day,Days,returning_loyal_customer_prob=.6):
-        self.days=Days
+    def __init__(self, products, mean_customers_per_day, days, returning_loyal_customer_prob=.6):
+        self.days = days
         #Initialise the data file to be output:
-        self.data=[["Date","Loyalty Number (0=Not on scheme)","Basket"]]
+        self.data = [["Date", "Loyalty Number (0 = Not on scheme)", "Basket"]]
         #A list of loyal customers:
-        loyal_customers=[]
+        loyal_customers = []
         for i in range(self.days):
             #Sample number of customers that day:
-            customers=poisson_sample(Mean_Customers_Per_Day)
+            customers = poisson_sample(mean_customers_per_day)
             for e in range(customers):
                 #Check whether customer is a returning loyal customer:
-                if len(loyal_customers)>0 and random.random()<returning_loyal_customer_prob:
+                if len(loyal_customers) > 0 and random.random() < returning_loyal_customer_prob:
                     #Pick random customer from loyal_customers:
-                    cust=random.choice(loyal_customers)
+                    cust = random.choice(loyal_customers)
                     #Generate new basket for customer:
-                    cust.new_basket(Products)
-                    loyalty_number=id(cust)
+                    cust.new_basket(products)
+                    loyalty_number = id(cust)
                 else:
                     #If customer is not a returning customer generate a new one:
-                    cust=Customer(Products)
+                    cust = Customer(products)
                     #Check if new customer signs up to loyalty scheme:
                     if cust.loyal:
                         loyal_customers.append(cust)
-                    loyalty_number=0
-                self.data.append([i+1,loyalty_number,cust.basket])
+                    loyalty_number = 0
+                self.data.append([i + 1, loyalty_number, cust.basket])
 
-    def create_file(self,filename="Sale_History.csv"):
+    def create_file(self, filename="Sale_History.csv"):
         #Write Sale_History file.
-        outfile=open(filename,"wb")
-        output=csv.writer(outfile)
+        outfile = open(filename, "wb")
+        output = csv.writer(outfile)
         for row in self.data:
             output.writerow(row)
         outfile.close()
         print "---------------------"
         print ""
-        print "Output file: %s created with %s baskets over %s days:"%(filename,len(self.data)-1,self.days)
+        print "Output file: %s created with %s baskets over %s days:" % (filename, len(self.data) - 1, self.days)
         print ""
         print "---------------------"
 
 """
 The above classes need a Stock Report as input. The following code imports "Stock_Report.csv" as default if no file name is passed at the command line.
 """
-files_in_directory=os.listdir(".")
+files_in_directory = os.listdir(".")
 
-if len(sys.argv)==1:
+if len(sys.argv) == 1:
     if "Stock_Report.csv" not in files_in_directory:
         print "-----"
         print ""
@@ -138,10 +138,10 @@ if len(sys.argv)==1:
         print ""
         print "-----"
     else:
-        Stock_Report="Stock_Report.csv"
+        stock_report = "Stock_Report.csv"
 else:
-    Stock_Report=argv[1]
+    stock_report = sys.argv[1]
 
-Products=import_file(Stock_Report)
-a=Sale_History(Products,1000,30)
+products = import_file(stock_report)
+a = Sale_History(products, 1000, 30)
 a.create_file()
